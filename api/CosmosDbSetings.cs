5735ce5ae2a68997f2e3737160d749bcdf561263
Satnam Sandhu <sandhu.satnam@outlook.com>
2024-11-03 15:59:14 -0500

    Add user interactions. (#10)

---
+++ api/CosmosDbSetings.cs
@@ -0,0 +1,4 @@
+public class CosmosDbSettings
+{
+    public string ConnectionString { get; set; }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,11 @@
+using api.Services;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 // Add CosmosDbService singleton to the service container
@@ -19,15 +22,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Health Track API V1");
+        c.DocumentTitle = "Health Track API Documentation";
+    });
 }
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.UseHttpsRedirection();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
