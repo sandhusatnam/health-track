48863965eb5c135d6ff39c7557466d24fe1f9bed
Satnam Sandhu <sandhu.satnam@outlook.com>
2024-11-03 18:32:49 +0000

    Clean up DB changes.

--- api/Program.cs
+++ api/Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<CosmosDbService>(serviceProvider =>
 {
     var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-    var cosmosDbSettings = configuration.GetSection("CosmosDb").Get<CosmosDbSettings>();
+    var connectionString = configuration["ConnectionStrings:MainDB"];
+    var cosmosDbSettings = new CosmosDbSettings { ConnectionString = connectionString };
     return new CosmosDbService(cosmosDbSettings);
 });
 
@@ -24,36 +25,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-public class CosmosDbSettings
-{
-    public string Account { get; set; }
-    public string Key { get; set; }
-    public string DatabaseName { get; set; }
 }
