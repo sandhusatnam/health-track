564e5d76bc5c95e7357028c1c4be754693b90c14
Satnam Sandhu <sandhu.satnam@outlook.com>
2024-12-04 19:15:34 -0500

    Update WaterLog.cs

--- api/Models/ALog/WaterLog.cs
+++ api/Models/ALog/WaterLog.cs
@@ -6,7 +6,6 @@ namespace api.Models.Log
     {
         public string? id { get; set; }
         public string userId { get; set; }
-        //public DateTime timestamp { get; set; }
         public string type { get; set; } = "water";
         public LogDetails details { get; set; }
 
